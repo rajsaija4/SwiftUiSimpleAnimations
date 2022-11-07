a7b6388379dd0785fe4fef0137868371bbcda490
Raj <rajsaija4@gmail.com>
2022-11-07 09:26:25 +0530

    Initial Commit

---
+++ DemoSwiftUi/DemoSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoSwiftUiApp.swift
+//  DemoSwiftUi
+//
+//  Created by Raj Saija on 07/11/22.
+//
+
+import SwiftUI
+
+@main
+struct DemoSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
